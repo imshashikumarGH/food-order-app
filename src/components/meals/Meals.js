af9f04c26fb1eab5cffaf9037fca54fce2d7774d
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-06 21:26:51 +0530

    Code Refactor

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -5,8 +5,8 @@ import MealsSummary from "./MealsSummary";
 const Meals = (props) => {
   return (
     <>
-      <AvailableMeals />
       <MealsSummary />
+      <AvailableMeals />
     </>
   );
 };
