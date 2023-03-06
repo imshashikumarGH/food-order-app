bb91c359b51859f9229f7a92e8a3bece49d42ce9
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-06 21:38:01 +0530

    Feature Meal Item Listing : Adding form to add meal to cart for list of meals

---
+++ src/components/meals/MealIteam/MealItemForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './MealItemForm.module.css'
+
+const MealItemForm = () => {
+
+    return <form className={classes.form}>
+        <input/>
+        <button>+ ADD</button>
+    </form>
+
+}
+
+export default MealItemForm; 
