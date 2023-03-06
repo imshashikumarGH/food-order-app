f127f60c1b244918d23931bd191183a6d1d63635
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-06 16:18:39 +0530

    Meals listing Feature : Adding Main html  in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <main>
+      <Meals></Meals>
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = (props) => {
+  return (
+    <>
+      <AvailableMeals />
+      <MealsSummary />
+    </>
+  );
+};
+
+export default Meals;
