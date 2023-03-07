5d5cb695f0d368bc4f117c69f38123baec01e99f
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-07 21:48:38 +0530

    Feature Cart : Adding modal for popup

---
+++ src/components/common/Modal.js
@@ -0,0 +1,31 @@
+import classes from "./Modal.module.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const protalId = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop />, protalId)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        protalId
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
