9a71dec8817372d12e4259642af02b68958a05eb
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-07 21:50:02 +0530

    Feature Cart Addreding Cart in app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header></Header>
       <main>
-      <Meals></Meals>
+        <Meals></Meals>
       </main>
     </React.Fragment>
   );
