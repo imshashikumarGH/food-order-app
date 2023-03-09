5632e0d6bc914a9ffaa32e9438fd243b950d608c
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-09 12:57:08 +0530

    Feature ItemInCart : code refactor

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import CartIcon from "../cart/CartIcon";
+import CartContext from "../store/Cart-Context";
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton =(props) =>{
+    const itemCtx = useContext(CartContext)
     return <button className={classes.button} onClick = {props.onShownCart}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{itemCtx.item.length}</span>
     </button>
 }
 export default HeaderCartButton
